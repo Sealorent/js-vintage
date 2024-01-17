baf3bf56cfda3e33b08654878d12f947c7275213
Sealorent <vikylorentseaputra@gmail.com>
2024-01-17 21:49:01 +0700

    push

--- js/product.js
+++ js/product.js
@@ -1,75 +1,108 @@
 const products = [
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/1.png',
+        title: 'Chaise TOKYO ',
+        sub: 'Teck',
+        dimensions: '76,5 x 54 x 53 cm',
+        materials: 'ART2485',
+        price: 'lot de 6 chaises 1320€ TTC'
     },
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/2.png',
+        title: 'tabouret de bar BRUT',
+        sub: 'Teck',
+        dimensions: '74,5 x 45 x 47 cm',
+        materials: 'ART3020',
+        price: 'lot de 3 tabourets 775€ TTC '
     },
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/3.png',
+        title: 'Chaise HIROSHIMA',
+        sub: 'Teck Carbone',
+        dimensions: '78,5 x 57 x 51 cm',
+        materials: 'ART2491',
+        price: 'lot de 8 chaises 2250€ TTC VENDU'
     },
+    // Table basse VERSAILLES ART2473 hêtre patiné métal finition laiton et rotin dimensions :120x120x41,5cm / 720€ TTC
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/4.jpg',
+        title: 'Table Basse',
+        sub: 'VERSAILLES',
+        dimensions: '120x120x41,5cm',
+        materials: 'ART2473 hêtre patiné métal finition laiton et rotin',
+        price: '720€ TTC'
     },
+    // abourets BETON ART2585 résine finition béton ciré dimensions : hauteur 44 diametre 35 / lot de 3 pièces 300€TTC
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/5.jpg',
+        title: 'Abourets',
+        sub: 'BETON',
+        dimensions: 'hauteur 44 diametre 35',
+        materials: 'ART2585 résine finition béton ciré',
+        price: 'lot de 3 pièces 300€ TTC'
     },
+    // Canapé ANVERS ART2731 housse en coton et jutte (housse neuve) dimensions :220X98X70cm 2150€TTC
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/6.png',
+        title: 'Canapé ',
+        sub: 'ANVERS',
+        dimensions: '220X98X70cm',
+        materials: 'ART2731 housse en coton et jutte (housse neuve)',
+        price: '2150€ TTC'
     },
+    // Luminaire BAMBOU set de trois tailles ART 3415/3416/3417 diamètre 60/80/120 440€TTC
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/7.jpg',
+        title: 'Luminaire',
+        sub: 'BAMBOU set de trois tailles',
+        dimensions: 'diamètre 60/80/120',
+        materials: 'ART 3415/3416/3417',
+        price: '440€ TTC'
     },
+    // Table basse LIT Ethiopien pièce unique antiquité ART3262 dimensions 100x180 environ hauteur 50 2400€TTC
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
+        image: './assets/product/8.jpg',
+        title: 'Table',
+        sub: 'basse LIT Ethiopien pièce unique antiquité',
+        dimensions: '100x180 environ hauteur 50',
+        materials: 'ART3262',
+        price: '2400€TTC    '
     },
+    // Luminaire Papouasie Small ART2998 diametre 60 / lot de 3 pieces 360€ TTC
     {
-        image: './assets/images/table.svg',
-        title: 'Table Basse Versailles',
-        dimensions: '120x80x50 cm',
-        quantity: '5',
-        materials: 'Chêne massif, verre',
-        price: '599 €'
-    }
+        image: './assets/product/9.jpg',
+        title: 'Luminaire',
+        sub: 'Papouasie Small',
+        dimensions: ' 60 ',
+        materials: 'ART2998',
+        price: 'lot de 3 pieces 360€ TTC'
+    },
+    //  Planche à découper OCEAN ART2303 Teck dimensions 50x20 lot de 10 planches 450€ TTC
+    {
+        image: './assets/product/10.png',
+        title: 'Planche à découper OCEAN',
+        sub: 'Teck',
+        dimensions: '50x20 ',
+        materials: 'ART2303',
+        price: 'lot de 10 planches 450€ TTC'
+    },
+    // Huilier Drop verre soufflé sur pièce de teck (pièce unique) ART2764 diametre 11 à 13cm 55€TTC
+    {
+        image: './assets/product/11.jpg',
+        title: 'Huilier Drop ',
+        sub: 'verre soufflé sur pièce de teck (pièce unique)',
+        dimensions: 'diametre 11 à 13cm',
+        materials: 'ART2764',
+        price: '55€ TTC'
+    },
+    // Set de 3 planches MAISON ART3210 /3211/3212 teck dimensions 25/40/54x15cm / 60€TTC
+    {
+        image: './assets/product/12.png',
+        title: 'Set de 3 planches MAISON' ,
+        sub: 'Teck',
+        dimensions: '25/40/54x15cm',
+        materials: 'ART3210 /3211/3212',
+        price: '60€ TTC'
+    },
     // Add more products here as needed
 ];
