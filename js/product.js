30c98b6a7745c34fd670edc653d27e407e073428
Sealorent <vikylorentseaputra@gmail.com>
2024-01-17 01:37:57 +0100

    mail

---
+++ js/product.js
@@ -0,0 +1,75 @@
+const products = [
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    },
+    {
+        image: './assets/images/table.svg',
+        title: 'Table Basse Versailles',
+        dimensions: '120x80x50 cm',
+        quantity: '5',
+        materials: 'Chêne massif, verre',
+        price: '599 €'
+    }
+    // Add more products here as needed
+];
